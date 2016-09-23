a66df1581683984ecab209277f29c0d51e05becf
AleksandrMishchaniuk <map.m@ua.fm>
2016-09-23 14:07:00 +0300

    added devise settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "todo-list-api_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'map-todo-list-api.herokuapp.com', protocol: 'https' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
